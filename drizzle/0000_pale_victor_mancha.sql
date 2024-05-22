df642eff79b6b01089d09f66e2137dfab8483e82
nazariyv <hi@naz.ooo>
2024-05-22 21:25:52 +0100

    feat: web3auth data to persist in db

--- drizzle/0000_pale_victor_mancha.sql
+++ drizzle/0000_pale_victor_mancha.sql
@@ -1,7 +1,12 @@
 CREATE TABLE IF NOT EXISTS "user" (
 	"id" serial NOT NULL,
 	"name" text NOT NULL,
-	"email" text,
+	"email" text NOT NULL,
+	"profileImage" text NOT NULL,
+	"typeOfLogin" text NOT NULL,
+	"aggregateVerifier" text NOT NULL,
+	"verifier" text NOT NULL,
+	"verifierId" text NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL
 );
--- src/lib/server/schema.ts
+++ src/lib/server/schema.ts
@@ -5,7 +5,12 @@ import { minLength, parse, string } from 'valibot';
 export const user = pgTable('user', {
   id: serial('id'),
   name: text('name').notNull(),
-  email: text('email'),
+  email: text('email').notNull(),
+  profileImage: text('profileImage').notNull(),
+  typeOfLogin: text('typeOfLogin').notNull(),
+  aggregateVerifier: text('aggregateVerifier').notNull(),
+  verifier: text('verifier').notNull(),
+  verifierId: text('verifierId').notNull(),
   createdAt: timestamp('created_at').notNull().defaultNow(),
   updatedAt: timestamp('updated_at')
     .notNull()
@@ -13,6 +18,8 @@ export const user = pgTable('user', {
     .$onUpdate(() => new Date())
 });
 
+// TODO: either validate or remove
+// since this is coming from web3Auth directly we probably don't need to validate?
 export const userInsertSchema = createInsertSchema(user, {
   name: string([minLength(2)])
 });
