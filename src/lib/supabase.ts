9fb180a1e7228f5779b7edee8177e697d9539c6d
naz <hi@naz.ooo>
2024-05-26 18:12:47 +0100

    chore: autosort

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
-import {
-  PUBLIC_SUPABASE_PROJECT_ID,
-  PUBLIC_SUPABASE_ANON_KEY
-} from '$env/static/public';
 import { createClient } from '@supabase/supabase-js';
+
+import {
+  PUBLIC_SUPABASE_ANON_KEY,
+  PUBLIC_SUPABASE_PROJECT_ID} from '$env/static/public';
 
 export const supabaseClient = createClient(`https://${PUBLIC_SUPABASE_PROJECT_ID}.supabase.co`, PUBLIC_SUPABASE_ANON_KEY);
