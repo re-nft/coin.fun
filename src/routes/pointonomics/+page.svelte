df365f24d1906c5f60ecf4576b84e790df580d71
nazariyv <hi@naz.ooo>
2024-05-27 15:38:18 +0100

    feat: pointonomics text

--- src/routes/pointonomics/+page.svelte
+++ src/routes/pointonomics/+page.svelte
@@ -1,8 +1,10 @@
 <div>
   <h1 class="martian-mono-extrabold">pointonomics</h1>
   <p>
-    There will be a total supply of 420,690,000,000 tokens (cough cough points)
+    There will be a total supply of 420,690,000,000 tokens (cough cough points).
   </p>
+
+  <br />
 
   <p>
     There are four groups of agents who are very important to us, who we shall
@@ -22,6 +24,22 @@
   <p>
     😎 <span class="martian-mono-medium">Chads</span> - people printing the memecoins
     🫰🤑
+  </p>
+  <br />
+
+  <p>TODO: come up with better names and descriptions</p>
+  <br />
+
+  <p>
+    People on the waitlist will be given exclusive access to coin.fun in the
+    first two weeks of launch. All fees accrued will be shared between these
+    accounts only
+  </p>
+  <br />
+
+  <p>
+    The earlier you claim and register on coin.fun, the higher multiplier on the
+    fees earned.
   </p>
 </div>
 
