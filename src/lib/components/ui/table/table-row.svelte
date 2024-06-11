9c6df1c119630ab9deca2e221ea24693e18d6415
Rommert Zijlstra <rommert.thomas@gmail.com>
2024-06-11 20:44:34 +0200

    chore: table row green border color

--- src/lib/components/ui/table/table-row.svelte
+++ src/lib/components/ui/table/table-row.svelte
@@ -1,23 +1,24 @@
 <script lang="ts">
-	import type { HTMLAttributes } from "svelte/elements";
-	import { cn } from "$lib/utils/ui.js";
+  import type { HTMLAttributes } from 'svelte/elements';
 
-	type $$Props = HTMLAttributes<HTMLTableRowElement> & {
-		"data-state"?: unknown;
-	};
+  import { cn } from '$lib/utils/ui.js';
 
-	let className: $$Props["class"] = undefined;
-	export { className as class };
+  type $$Props = HTMLAttributes<HTMLTableRowElement> & {
+    'data-state'?: unknown;
+  };
+
+  let className: $$Props['class'] = undefined;
+  export { className as class };
 </script>
 
 <tr
-	class={cn(
-		"border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
-		className
-	)}
-	{...$$restProps}
-	on:click
-	on:keydown
+  class={cn(
+    'border-b border-b-brand-green-light transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted',
+    className
+  )}
+  {...$$restProps}
+  on:click
+  on:keydown
 >
-	<slot />
+  <slot />
 </tr>
