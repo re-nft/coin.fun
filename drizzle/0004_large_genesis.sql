3b37fa717e097eea8b6b94fc09a3750905cf1bdb
Rommert Zijlstra <rommert.thomas@gmail.com>
2024-06-18 23:45:27 +0200

    chore: add tweets table

---
+++ drizzle/0004_large_genesis.sql
@@ -0,0 +1,91 @@
+CREATE TABLE IF NOT EXISTS "tweets" (
+	"id" bigint PRIMARY KEY NOT NULL,
+	"user_id" uuid NOT NULL,
+	"full_text" text NOT NULL,
+	"entities" json NOT NULL,
+	"quoted_id" bigint,
+	"reply_to_id" bigint,
+	"retweeted_id" bigint,
+	"favorite_count" integer,
+	"quote_count" integer,
+	"reply_count" integer,
+	"retweet_count" integer,
+	"created_at" timestamp NOT NULL
+);
+--> statement-breakpoint
+DROP INDEX IF EXISTS "user_idx";--> statement-breakpoint
+DROP INDEX IF EXISTS "quest_idx";--> statement-breakpoint
+DROP INDEX IF EXISTS "acquired_at_idx";--> statement-breakpoint
+ALTER TABLE "profiles" ADD COLUMN "twitter_user_id" varchar;--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "tweets" ADD CONSTRAINT "tweets_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "auth"."users"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "tweets" ADD CONSTRAINT "tweets_quoted_id_tweets_id_fk" FOREIGN KEY ("quoted_id") REFERENCES "public"."tweets"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "tweets" ADD CONSTRAINT "tweets_reply_to_id_tweets_id_fk" FOREIGN KEY ("reply_to_id") REFERENCES "public"."tweets"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "tweets" ADD CONSTRAINT "tweets_retweeted_id_tweets_id_fk" FOREIGN KEY ("retweeted_id") REFERENCES "public"."tweets"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "tweets_user_idx" ON "tweets" USING btree ("user_id");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "tweets_quoted_id_idx" ON "tweets" USING btree ("quoted_id");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "tweets_replied_to_id_idx" ON "tweets" USING btree ("reply_to_id");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "tweets_retweeted_id_idx" ON "tweets" USING btree ("retweeted_id");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "tweets_created_at_idx" ON "tweets" USING btree ("created_at");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "points_user_idx" ON "points" USING btree ("user_id");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "points_quest_idx" ON "points" USING btree ("quest_id");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "points_acquired_at_idx" ON "points" USING btree ("acquired_at");--> statement-breakpoint
+CREATE UNIQUE INDEX IF NOT EXISTS "profiles_email_idx" ON "profiles" USING btree ("email");--> statement-breakpoint
+CREATE UNIQUE INDEX IF NOT EXISTS "profiles_twitter_user_id_idx" ON "profiles" USING btree ("twitter_user_id");--> statement-breakpoint
+CREATE UNIQUE INDEX IF NOT EXISTS "profiles_user_name_idx" ON "profiles" USING btree ("user_name");--> statement-breakpoint
+ALTER TABLE "profiles" ADD CONSTRAINT "profiles_email_unique" UNIQUE("email");--> statement-breakpoint
+ALTER TABLE "profiles" ADD CONSTRAINT "profiles_user_name_unique" UNIQUE("user_name");--> statement-breakpoint
+ALTER TABLE "profiles" ADD CONSTRAINT "profiles_twitter_user_id_unique" UNIQUE("twitter_user_id");
+
+-- Backfill twitter_user_id
+UPDATE public.profiles AS profiles
+  SET twitter_user_id = users.raw_user_meta_data ->> 'provider_id'
+  FROM auth.users AS users
+  WHERE profiles.id = users.id;
+
+-- Update on_auth_user_created trigger handler to include provider_id
+-- as the twitter_user_id from now on.
+create or replace function public.handle_new_user()
+returns trigger
+language plpgsql
+security definer set search_path = ''
+as $$
+begin
+  insert into public.profiles (
+    id,
+    avatar,
+    display_name,
+    email,
+    twitter_user_id,
+    user_name
+  )
+  values (
+    new.id,
+    new.raw_user_meta_data ->> 'avatar_url',
+    new.raw_user_meta_data ->> 'full_name',
+    new.raw_user_meta_data ->> 'email',
+    new.raw_user_meta_data ->> 'provider_id',
+    new.raw_user_meta_data ->> 'user_name'
+  );
+  return new;
+end;
+$$;
--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,11 +1,15 @@
 import {
+  type AnyPgColumn,
   bigint,
   index,
+  integer,
+  json,
   pgSchema,
   pgTable,
   primaryKey,
   text,
   timestamp,
+  uniqueIndex,
   uuid,
   varchar
 } from 'drizzle-orm/pg-core';
@@ -16,29 +20,44 @@ const users = authSchema.table('users', {
   id: uuid('id').primaryKey()
 });
 
-export const profiles = pgTable('profiles', {
-  id: uuid('id')
-    .primaryKey()
-    .references(() => users.id, { onDelete: 'cascade' })
-    .notNull(),
+export const profiles = pgTable(
+  'profiles',
+  {
+    id: uuid('id')
+      .primaryKey()
+      .references(() => users.id, { onDelete: 'cascade' })
+      .notNull(),
 
-  avatar: text('avatar'),
-  email: text('email').notNull(),
-  userName: text('user_name').notNull(),
-  displayName: text('display_name'),
+    avatar: text('avatar'),
+    email: text('email').unique().notNull(),
+    userName: text('user_name').unique().notNull(),
+    displayName: text('display_name'),
 
-  // name: text('name').notNull(),
-  // typeOfLogin: text('typeOfLogin').notNull(),
-  // aggregateVerifier: text('aggregateVerifier').notNull(),
-  // verifier: text('verifier').notNull(),
-  // verifierId: text('verifier_id'),
+    // TODO notNull()
+    twitterUserId: varchar('twitter_user_id').unique(),
 
-  createdAt: timestamp('created_at').notNull().defaultNow(),
-  updatedAt: timestamp('updated_at')
-    .notNull()
-    .defaultNow()
-    .$onUpdate(() => new Date())
-});
+    // name: text('name').notNull(),
+    // typeOfLogin: text('typeOfLogin').notNull(),
+    // aggregateVerifier: text('aggregateVerifier').notNull(),
+    // verifier: text('verifier').notNull(),
+    // verifierId: text('verifier_id'),
+
+    createdAt: timestamp('created_at').notNull().defaultNow(),
+    updatedAt: timestamp('updated_at')
+      .notNull()
+      .defaultNow()
+      .$onUpdate(() => new Date())
+  },
+  (table) => {
+    return {
+      emailIdx: uniqueIndex('profiles_email_idx').on(table.email),
+      twitterUserIdIdx: uniqueIndex('profiles_twitter_user_id_idx').on(
+        table.twitterUserId
+      ),
+      userNameIdx: uniqueIndex('profiles_user_name_idx').on(table.userName)
+    };
+  }
+);
 
 export const points = pgTable(
   'points',
@@ -52,12 +71,51 @@ export const points = pgTable(
   },
   (table) => {
     return {
-      userIdx: index('user_idx').on(table.userId),
-      questIdx: index('quest_idx').on(table.questId),
-      acquiredAtIdx: index('acquired_at_idx').on(table.acquiredAt),
+      userIdx: index('points_user_idx').on(table.userId),
+      questIdx: index('points_quest_idx').on(table.questId),
+      acquiredAtIdx: index('points_acquired_at_idx').on(table.acquiredAt),
       pk: primaryKey({
         columns: [table.userId, table.questId, table.acquiredAt]
       })
+    };
+  }
+);
+
+export const tweets = pgTable(
+  'tweets',
+  {
+    id: bigint('id', { mode: 'bigint' }).primaryKey(),
+    userId: uuid('user_id')
+      .references(() => users.id, { onDelete: 'cascade' })
+      .notNull(),
+
+    fullText: text('full_text').notNull(),
+    entities: json('entities').notNull(),
+
+    quotedId: bigint('quoted_id', { mode: 'bigint' }).references(
+      (): AnyPgColumn => tweets.id
+    ),
+    repliedToId: bigint('reply_to_id', { mode: 'bigint' }).references(
+      (): AnyPgColumn => tweets.id
+    ),
+    retweetedId: bigint('retweeted_id', { mode: 'bigint' }).references(
+      (): AnyPgColumn => tweets.id
+    ),
+
+    favoriteCount: integer('favorite_count'),
+    quoteCount: integer('quote_count'),
+    replyCount: integer('reply_count'),
+    retweetCount: integer('retweet_count'),
+
+    createdAt: timestamp('created_at').notNull()
+  },
+  (table) => {
+    return {
+      userIdx: index('tweets_user_idx').on(table.userId),
+      quotedIdIdx: index('tweets_quoted_id_idx').on(table.quotedId),
+      repliedToIdIdx: index('tweets_replied_to_id_idx').on(table.repliedToId),
+      retweetedIdIdx: index('tweets_retweeted_id_idx').on(table.retweetedId),
+      createdAtIdx: index('tweets_created_at_idx').on(table.createdAt)
     };
   }
 );
