8f16dc59446ac6266952c9dde67e866ddc9efd08
Rommert Zijlstra <rommert.thomas@gmail.com>
2024-06-21 20:56:21 +0200

    chore: merge points with tweets

--- src/lib/components/quests/0003-daily-tweet.svelte
+++ src/lib/components/quests/0003-daily-tweet.svelte
@@ -5,15 +5,20 @@
 
   export let id: string;
   export let status: QuestStatus;
-  export let tweets: TweetSelect[];
+  export let tweets: (TweetSelect & { points?: number })[];
 </script>
 
 <Quest {...$$restProps} {status}>
   <div class="flex flex-col gap-4" slot="content">
-    {#each tweets as status (status.id)}
-      <p>
-        {status.fullText}
-      </p>
-    {/each}
+    {#if tweets?.length}
+      {#each tweets as status (status.id)}
+        <p>
+          {#if status.points}
+            <span class="text-brand-yellow">{status.points} points: </span>
+          {/if}
+          {status.fullText}
+        </p>
+      {/each}
+    {/if}
   </div>
 </Quest>
--- src/lib/server/quests/0003-daily-tweet.ts
+++ src/lib/server/quests/0003-daily-tweet.ts
@@ -1,4 +1,4 @@
-import { and, eq, sql } from 'drizzle-orm';
+import { and, eq, or, sql } from 'drizzle-orm';
 
 import { OnError, Quest } from '$lib/quests';
 import { db, points, tweets } from '$lib/server/db';
@@ -20,13 +20,37 @@ export class Quest0003DailyTweet extends Quest {
 
   async toJSON() {
     const meta = await super.toJSON();
+
     if (!this.userId) return meta;
+
+    const userTweets = await db
+      .select()
+      .from(tweets)
+      .where(eq(tweets.userId, this.userId));
+
+    const tweetPoints = await db
+      .select()
+      .from(points)
+      .where(
+        or(
+          ...userTweets.map(({ userId, createdAt }) =>
+            and(
+              eq(points.acquiredAt, createdAt),
+              eq(points.questId, this.id),
+              eq(points.userId, userId)
+            )
+          )
+        )
+      );
+
     return {
       ...meta,
-      tweets: await db
-        .select()
-        .from(tweets)
-        .where(eq(tweets.userId, this.userId))
+      tweets: userTweets.map((tweet) => ({
+        ...tweet,
+        points: tweetPoints.find(
+          ({ acquiredAt }) => acquiredAt.getTime() === tweet.createdAt.getTime()
+        )?.points
+      }))
     };
   }
 }
