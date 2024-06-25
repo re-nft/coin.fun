f23e72888b7012aa0cc00bf1e375b484b0a47bc5
Rommert Zijlstra <rommert.thomas@gmail.com>
2024-06-25 21:33:28 +0200

    chore: style updates

--- src/lib/components/quests/0003-daily-tweet.svelte
+++ src/lib/components/quests/0003-daily-tweet.svelte
@@ -3,23 +3,30 @@
   import ChevronsDownUp from 'lucide-svelte/icons/chevrons-down-up';
   import ChevronsUpDown from 'lucide-svelte/icons/chevrons-up-down';
   import { onMount } from 'svelte';
-  import { slide } from 'svelte/transition';
+  import { scale, slide } from 'svelte/transition';
 
   import { enhance } from '$app/forms';
   import { goto } from '$app/navigation';
   import Quest from '$lib/components/Quest.svelte';
   import type { QuestCallError } from '$lib/quests';
   import type { Profile, TweetSelect } from '$lib/server/db';
+  import { getUTCDayStart } from '$lib/utils/date';
   import { suffix } from '$lib/utils/number';
   import { cn } from '$lib/utils/ui';
 
   import { Button } from '../ui/button';
 
+  type Tweet = TweetSelect & { points?: number };
+  type TweetsByDate = Map<Date, Tweet[]>;
+
   export let id: string;
   export let locale: string | undefined = undefined;
   export let profile: Profile | undefined = undefined;
+  export let tweets: Tweet[];
+
+  // We're snooping the title from the props here because we don't actually
+  // want to use it for this component. Maybe rewrite Quest.svelte some point.
   export let title: string | undefined = undefined;
-  export let tweets: (TweetSelect & { points?: number })[];
 
   const intlDate = new Intl.DateTimeFormat(locale, {
     dateStyle: 'long'
@@ -29,6 +36,32 @@
     'Get poor quicker. Fill my bags @coindotfun'
   );
 
+  // Twitter list setup
+  $: [tweetsByDate] = tweets.reduce<
+    [tweetsByDate: TweetsByDate, prevDate: Date]
+  >(
+    ([tweetsByDate, prevDate], status) => {
+      const nextDate = getUTCDayStart(status.createdAt);
+      let currDate = prevDate;
+
+      while (currDate >= nextDate) {
+        tweetsByDate.set(new Date(currDate), []);
+        currDate.setDate(currDate.getDate() - 1);
+      }
+
+      tweetsByDate.set(currDate, [
+        ...(tweetsByDate.get(currDate) ?? []),
+        status
+      ]);
+
+      return [tweetsByDate, nextDate];
+    },
+    [new Map<Date, Tweet[]>(), getUTCDayStart()]
+  );
+
+  $: collapsedDates = Array.from(tweetsByDate.keys());
+
+  // Form things
   let formData: { id: string } | { errors: QuestCallError[] } | undefined =
     undefined;
   let formStatus: ActionResult['type'] | 'idle' | 'pending' = 'idle';
@@ -38,12 +71,12 @@
 
   $: data = formData && 'id' in formData ? formData.id : undefined;
   $: errors = formData && 'errors' in formData ? formData?.errors : undefined;
-
   $: {
     if (prevUrl !== url) errors = undefined;
     prevUrl = url;
   }
 
+  // Collapse code
   let collapsed = true;
 
   $: {
@@ -68,12 +101,7 @@
   style="--color: hsl(var(--color-red));"
 >
   <header class="flex w-full items-center gap-4" slot="header">
-    Quest 3 daily tweets:
-    {#if profile?.characterS1 === 'normie'}
-      <span class="text-[--color]">150k</span>
-    {:else if profile?.characterS1 === 'heftie'}
-      <span class="text-[--color]">100k or 200k</span>
-    {/if}
+    {title}
 
     <button
       class="ml-auto mr-4 flex size-8 items-center justify-center rounded border border-muted"
@@ -82,7 +110,7 @@
       }}
       type="button"
     >
-      {#if collapsed}
+      {#if !collapsed}
         <ChevronsDownUp size={16} />
       {:else}
         <ChevronsUpDown size={16} />
@@ -90,40 +118,79 @@
     </button>
   </header>
 
-  <div class="flex min-h-0 grow flex-col gap-8" slot="content">
+  <div class="flex min-h-0 grow flex-col gap-8 text-left" slot="content">
     {#if tweets?.length}
-      <div
+      <ol
         class="flex min-h-0 grow flex-col overflow-y-auto border-b border-b-[--color] bg-[#15202B]"
       >
-        {#each tweets as status (status.id)}
-          {#each tweets as status (status.id)}
-            <div class={cn('p-2', status.points && 'bg-brand-yellow/10')}>
-              {#if status.points}
-                <p>
-                  This gib <span class="text-brand-yellow"
-                    >{suffix(status.points)}</span
-                  >
-                </p>
-              {/if}
-              <blockquote
-                class="twitter-tweet text-brand-beige/40"
-                data-dnt="true"
-                data-theme="dark"
+        {#each Array.from(tweetsByDate.entries()) as [date, tweets] (date)}
+          <li class="w-full">
+            <h3 class="flex items-center gap-2 text-muted-foreground/50">
+              <time
+                class="flex h-12 flex-none items-center gap-2 whitespace-nowrap before:basis-4 before:border-b before:border-b-muted-foreground/50"
+                datetime={date.toISOString()}>{intlDate.format(date)}</time
               >
-                <p class="my-2 text-brand-beige">
-                  {status.fullText}
-                </p>
-                &mdash; {profile?.displayName} (@{profile?.userName})
-                <a
-                  class="text-brand-orange underline transition hover:text-brand-yellow"
-                  href="https://twitter.com/{profile?.userName}/status/{status.id}?ref_src=twsrc%5Etfw"
-                  >{intlDate.format(status?.createdAt)}</a
-                >
-              </blockquote>
-            </div>
-          {/each}
+              <span
+                class="flex h-12 grow items-center gap-2 before:basis-4 before:border-b before:border-b-muted-foreground/50 after:flex-1 after:border-b after:border-b-muted-foreground/50"
+                class:text-brand-yellow={tweets.length}
+                class:text-glow-red={tweets.length}
+                class:text-glow-black={!tweets.length}
+                >{tweets.reduce(
+                  (total, { points }) => total + (points ?? 0),
+                  0
+                )}</span
+              >
+              <span class="text-sm font-normal">({tweets.length})</span>
+              <button
+                class="ml-auto mr-4 flex size-8 items-center justify-center rounded border border-muted"
+                on:click={() => {
+                  collapsedDates =
+                    collapsedDates.includes(date) ?
+                      collapsedDates.filter((_) => _ !== date)
+                    : [...collapsedDates, date];
+                  // @ts-expect-error TODO: types
+                  window?.twttr?.widgets?.load?.();
+                }}
+                type="button"
+              >
+                {#if !collapsedDates.includes(date)}
+                  <ChevronsDownUp size={16} />
+                {:else}
+                  <ChevronsUpDown size={16} />
+                {/if}
+              </button>
+            </h3>
+            {#if !collapsedDates.includes(date)}
+              <div transition:slide>
+                {#each tweets as status (status.id)}
+                  <div
+                    class={cn(
+                      'px-4',
+                      status.points && 'ml-4 border-l-2 border-l-brand-yellow'
+                    )}
+                  >
+                    <blockquote
+                      class="twitter-tweet text-brand-beige/40"
+                      data-dnt="true"
+                      data-theme="dark"
+                    >
+                      <p class="my-2 text-brand-beige">
+                        {status.fullText}
+                      </p>
+                      &mdash; {profile?.displayName} (@{profile?.userName})
+                      <a
+                        class="text-brand-orange underline transition hover:text-brand-yellow"
+                        href="https://twitter.com/{profile?.userName}/status/{status.id}?ref_src=twsrc%5Etfw"
+                        >{intlDate.format(status?.createdAt)}</a
+                      >
+                    </blockquote>
+                  </div>
+                {/each}
+              </div>
+            {/if}
+          </li>
         {/each}
-      </div>
+      </ol>
     {:else}
       <div class="flex flex-col items-center gap-8 p-8">
         <p class="text-brand-yellow">
