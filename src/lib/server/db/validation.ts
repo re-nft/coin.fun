911942aed9e3f2bed5c5522299cb761126ba816a
Rommert Zijlstra <rommert.thomas@gmail.com>
2024-07-01 13:04:10 +0200

    chore: update validations to trim strings

--- src/lib/server/db/validation.ts
+++ src/lib/server/db/validation.ts
@@ -9,19 +9,27 @@ export const CoinInsertSchema = createInsertSchema(coins, {
   createdBy: v.uuid(),
   description: v.pipe(
     v.string(),
+    v.trim(),
     v.nonEmpty('Description cannot be empty. You need to really sell it.')
   ),
   media: v.string(),
-  name: v.pipe(v.string(), v.nonEmpty('Name cannot be empty.')),
+  name: v.pipe(v.string(), v.trim(), v.nonEmpty('Name cannot be empty.')),
   symbol: v.pipe(
     v.string(),
+    v.trim(),
     v.nonEmpty('Ticker cannot be empty.'),
     v.regex(/[a-zA-Z]+/, 'Ticker cannot contain numbers or symbols.'),
     v.toUpperCase()
   ),
-  telegram: v.nullish(
-    v.pipe(v.string(), v.url('Telegram must be a valid URL.'))
+  telegram: v.pipe(
+    v.string(),
+    v.trim(),
+    v.url('Telegram must be a valid URL.')
   ),
-  twitter: v.nullish(v.pipe(v.string(), v.url('Twitter must be a valid URL.'))),
-  website: v.nullish(v.pipe(v.string(), v.url('Webiste must be a valid URL.')))
+  twitter: v.nullish(
+    v.pipe(v.string(), v.trim(), v.url('Twitter must be a valid URL.'))
+  ),
+  website: v.nullish(
+    v.pipe(v.string(), v.trim(), v.url('Webiste must be a valid URL.'))
+  )
 });
